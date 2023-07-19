6ca5eb84fb9dfb2ba578de3935b37c325d216f0f
iambalwant <balwantverma55555@gmail.com>
2023-07-19 15:39:36 +0530

    UI and js update

--- diamondexch/diamond/static/js.js
+++ diamondexch/diamond/static/js.js
@@ -1,30 +1,41 @@
-const input = document.querySelectorAll(".main_form_input"),
+const inputs = document.querySelectorAll(".main_form_input"),
 button = document.querySelector(".otp_button");
 
-//iterate iver all inouts
-// input.forEach((input, index1) => {
-//     input.addEventListener("keyup", () => {
-      //this code gets the current input element and stores it in the current Input variable 
-      //this is codes gets the next sibling elemnt of the current input elemnt and stores it in the next
+// iterate iver all inouts
+inputs.forEach((input, index1) => {
+    input.addEventListener("keyup", (e) => {
+      // this code gets the current input element and stores it in the current Input variable 
+      // this is codes gets the next sibling elemnt of the current input elemnt and stores it in the next
 
-//  const currentInput = input,
-//   nextInput = input.nextElementSibling,
-//   prevInput = input.previousElementSibling;
+ const currentInput = input,
+  nextInput = input.nextElementSibling,
+  prevInput = input.previousElementSibling;
 
-//   //if the value has more then one charater then clear it
-//    if (currentInput.value.length > 1){
-//     currentInput.value = "";
-//     return;
-//    }
-//    if(nextInput && nextInput.hasAttribute("disabled") && currentInput.value !== ""){
-//     nextInput.removeAttribute("disabled");
-//     nextInput.focus();
-//    }
-//     });
-// });
+  //if the value has more then one charater then clear it
+   if (currentInput.value.length > 1){
+    currentInput.value = "";
+    return;
+   }
+   if(nextInput && nextInput.hasAttribute("disabled") && currentInput.value !== ""){
+    nextInput.removeAttribute("disabled");
+    nextInput.focus();
+   }
+ //if backspace key is passed
+ if (e.key === "Backspace"){
+  console.log(inputs);
+  inputs.forEach((input, index2) => {
+   if (index1 <= index2 && prevInput){
+    input.setAttribute("disabled", true);
+    currentInput.value = "";
+    prevInput.focus();
+   }
+  });
+ }
+    });
+});
 
 //focus the first input which index is 0 window load
-window.addEventListener("load", () => input[0].focus());
+window.addEventListener("load", () => inputs[0].focus());
 
 
 const form = document.getElementById('form');
